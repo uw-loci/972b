dd4c97a157df1271ade6ac92b58707492d5105d6
Michael Laffin <mslaffin@wisc.edu>
2023-12-26 21:57:26 -0600

    Added example for changing baud rate.

--- examples/972b_model_number_query/972b_model_number_query.ino
+++ examples/972b_model_number_query/972b_model_number_query.ino
@@ -1,10 +1,22 @@
 #include <972b.h>
 
 void setup() {
-    Serial.begin(9600);   // initialize PC COM interface
-    Serial2.begin(9600);  // initialize UART-RS486 transceiver interface
+    Serial.begin(9600);   // initialize PC COM interface - pins 0(RX), 1(TX)
+    Serial2.begin(9600);  // initialize UART-RS486 transceiver interface - pins 17(RX2), 16(TX2)
    
-    sendCommand("MD?"); // Query device model number
+   
+    String response;
+
+    sendCommand("BR?"); // Query the default BR
+    response = readResponse();
+    Serial.println("Default Baud Rate: " + response);
+    
+	changeBaudRate("19200");
+    delay(1000); // Delay allow sensor to configure
+
+    sendCommand("BR?"); // Query the new BR
+    response = readResponse();
+    Serial.println("Changed Baud Rate: " + response);
 }
 
 void loop() {
